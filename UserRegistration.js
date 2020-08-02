70d1f17b3e6a72787438d1874a5a337e8eaed92d
Amrut <amrutpanda7789@gmail.com>
2020-08-02 23:07:28 +0530

    ADDED: Code to validate email address

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,28 +1,36 @@
 const prompt = require('prompt-sync')();
 
 let namePattern = RegExp("^[A-Z]{1}[A-Za-z]{2}");
+let emailPattern = RegExp("^[0-9a-zA-Z]+([-_+.][0-9a-zA-Z]+)*@[0-9A-Za-z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{3})*$");
 
 function checkPattern(input, inputPattern) {
-		return inputPattern.test(input);
+		 return inputPattern.test(input);
 }
 
 function getFirstName(){
     let firstName = prompt("Enter First Name: ");
     if(!checkPattern(firstName, namePattern)){
         console.log("Enter Valid First Name");
-        getFirstName();
+			getFirstName();
     }
-    return;
 }
 
 function getLastName(){
     let lastName = prompt("Enter Last Name: ");
     if(!checkPattern(lastName, namePattern)){
         console.log("Enter Valid Last Name");
-        getLastName();
+			getLastName();
     }
-    return
+}
+
+function getEmail(){
+    let email = prompt("Enter Email: ");
+    if(!checkPattern(email, emailPattern)){
+        console.log("Enter Valid Email");
+			getEmail();
+    }
 }
 
 getFirstName();
 getLastName();
+getEmail();
